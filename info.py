3431c3957932a768c63dd6a45ca76a1c32dcf522
comfymoviez <149220944+comfymoviez@users.noreply.github.com>
2024-02-02 19:21:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,6 +51,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://comfymoviez:comfymoviez@cluster0.fuacugq.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "comfymoviez")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
